46a62e2e0d5051219c00eaecf0cc76eae5186598
Youssef Elsayed <youssef.official.2411@gmail.com>
2026-02-18 05:00:43 -1000

    Update HomePage.tsx

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -36,7 +36,7 @@ interface HomePageProps {
   projectsLoading?: boolean;
 }
 
-const MAX_PROMPT_LENGTH = 2000;
+const MAX_PROMPT_LENGTH = 30000;
 
 export const HomePage: React.FC<HomePageProps> = ({
   onStartBuilding,
